ea41207e69d656d3ef8f6389125267874754a6c2
Leon <info@leon.vankammen.eu>
2017-04-19 13:58:47 +0200

    minor bugfix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var decircularize = require('decircularize')
 JSON._stringify = JSON.stringify
-JSON.stringify = >() { 
+JSON.stringify = function() { 
 	arguments[0] = decircularize(arguments[0], {
-		onCircular: >(){ return ? }
+		onCircular: function(){ return undefined }
 	}) 
 	return JSON._stringify.apply(JSON, arguments) 
 }         
